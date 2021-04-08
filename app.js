41e0c0caae26ad9990864ad7074fedc0cc495576
trovtle basnet <basnet.trovtle@gmail.com>
2021-04-08 22:46:37 +1000

    Added build foler

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,14 @@ app.use(cors());
 app.use('/api/user', require('./routes/user'));
 app.use('/api/chatroom', require('./routes/chatroom'));
 app.use('/api/message', require('./routes/message'));
+
+if (process.env.NODE_ENV !== 'DEVELOPMENT') {
+	app.use(express.static(path.join(__dirname, '/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+	);
+}
 
 //setup Error Handlers
 const errorHandlers = require('./handlers/errorHandlers');
